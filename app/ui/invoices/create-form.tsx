4645876c766f879a59c35df8a2aab2ca60cbb463
Ebrima Jallow <ebrimamaubeh7@gmail.com>
2025-10-12 11:33:47 +0200

    I skipped the authentication section of this demo. I will continue with metadata

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -33,7 +33,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
               aria-describedby="customer-error"
-            // required
+              required
             >
               <option value="" disabled>
                 Select a customer
@@ -70,7 +70,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              // required
+                required
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -91,7 +91,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
-                // required
+                  required
                 />
                 <label
                   htmlFor="pending"
@@ -107,7 +107,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="paid"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
-                // required
+                  required
                 />
                 <label
                   htmlFor="paid"
